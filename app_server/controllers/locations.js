a6dcf8437ff1dd71aee636f529b70fd1cc3831a1
Mike Murray <michaelbrazell@gmail.com>
2015-12-31 11:55:51 -0500

    Set up data inside controller, update homepage view to pull data

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
-/* Get 'HOME' page */
+/* Get 'HOME/Location List' page */
 module.exports.homelist = function(req, res) {
-	res.render('locations-list', { title: 'Home' });
+	res.render('locations-list', {
+ 		title: 'Locater - Find a place to work with wifi',
+ 		pageHeader: {
+ 			title: 'Locater',
+ 			strapline: 'Find places to work with wifi near you!'
+ 		},
+ 		locations: [{
+ 			name: 'Starcups',
+ 			address: '125 High Street, Reading, MA',
+ 			rating: 3,
+ 			facilities: ['Hot drinks', 'Nice toilets', 'Pleasant food'],
+ 			distance: '100m'
+ 		},{
+ 			name: 'MathWorks',
+ 			address: '3 Apple Hill Rd, Natick MA',
+ 			rating: 4,
+ 			facilities: ['Computers', 'Facilities'],
+ 			distance: '2103m'
+ 		},{
+ 			name: 'McDonalds',
+ 			address: '88 Parkton Ave, Worcester, MA',
+ 			rating: 2,
+ 			facilities: ['Happy Meals', 'Burgers', 'Nuggets', 'Coffee'],
+ 			distance: '54m'
+ 		}]
+ 	});
 };
 
 /* Get 'LOCATION INFO' Page */
