bf948c9b90631ba4be2205b9e00ae4d27c2ee466
Mike Murray <michaelbrazell@gmail.com>
2016-01-04 10:12:40 -0500

    Update all views and controllers with data fed from the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -6,6 +6,7 @@ module.exports.homelist = function(req, res) {
  			title: 'Locater',
  			strapline: 'Find places to work with wifi near you!'
  		},
+ 		sidebar: "Looking for wifi and a seat?  Locater helps you find places o work when out and about.  Perhaps with coffee, cake or a pint?  Let Locater help you find the place you're looking for.",
  		locations: [{
  			name: 'Starcups',
  			address: '125 High Street, Reading, MA',
@@ -30,10 +31,52 @@ module.exports.homelist = function(req, res) {
 
 /* Get 'LOCATION INFO' Page */
 module.exports.locationInfo = function(req, res) {
-	res.render('locations-info', { title: 'Location Info' });
+	res.render('locations-info', { 
+		title: 'Starcups - Locater',
+		pageHeader: { title: 'Starcups'	},
+		sidebar: {
+			context: 'is on Locater because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			callToAction: 'If you\'ve been here and you like it, please leave a review.'
+		},
+		location: {
+			name: 'Starcups',
+			address: '125 High Street, Reading, MA',
+ 			rating: 3,
+ 			facilities: ['Hot drinks', 'Nice toilets', 'Pleasant food'],
+ 			coords: {lat: 42.262593, lng: -71.802293},
+ 			openingTimes: [{
+ 				days: 'Monday - Friday',
+ 				opening: '7:00am',
+ 				closing: '7:00pm',
+ 				closed: false
+ 			},{
+ 				days: 'Saturday',
+ 				opening: '8:00am',
+ 				closing: '5:00pm',
+ 				closed: false
+ 			},{
+ 				days: 'Sunday',
+ 				closed: true
+ 			}],
+ 			reviews: [{
+ 				author: 'Simon Holmes',
+ 				rating: 5,
+ 				timestamp: '16 July 2013',
+ 				reviewText: 'What a great place, I really can\'t say enough things!'
+ 			},{
+ 				author: 'Mike Jones',
+ 				rating: 2,
+ 				timestamp: '15 May 2015',
+ 				reviewText: 'This place was just okay.'
+ 			}]
+		}
+	});
 };
 
 /* Get 'ADD NEW' Page */
 module.exports.addReview = function(req, res) {
-	res.render('location-review-form', { title: 'Add review' });
+	res.render('location-review-form', { 
+		title: 'Review Starcups on Locater' ,
+		pageHeader: { title: 'Review Starcups'}
+	});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res) { // does this need to be (req, res, next)?
-	res.render('generic-text', { title: 'About'});
+	res.render('generic-text', 
+		{ title: 'About Locater',
+		content: 'Lorem ipsum dolor sit amet, eu eos habeo ipsum adipiscing, vis no summo primis sensibus. Vim persecuti reprimique ex, ne odio singulis sed. Dicit elaboraret ullamcorper has in. Mel ad inani omnium, ea sea diam volumus interpretaris. \n\n Ut vim meliore prodesset quaerendum. Id probo nullam aperiam ius, quo dolor viderer fierent ei. Ornatus torquatos qui ea, at eum gubergren consetetur. Vis simul audire definiebas an, vix scripta liberavisse et. Inani errem ut vel, te per vocent fierent convenire, alii vivendo disputationi cu ius.  \n\nTe tollit scripserit contentiones sed, et dicam pertinacia mel. Id mei tale solum interpretaris. Rebum iudico viderer te sit. Ne commodo percipit complectitur pro. Eros melius civibus et vim, vix ne habemus commune contentiones.'
+	});
 };
