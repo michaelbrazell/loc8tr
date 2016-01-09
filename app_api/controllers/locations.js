4087254a91ec0f7edb5b7c7186c20c9a21dd5367
Mike Murray <michaelbrazell@gmail.com>
2016-01-09 10:24:26 -0500

    Add in geolocation functions

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -4,10 +4,62 @@ var Loc = mongoose.model('Location');
 var sendJsonResponse = function(res, status, content) {
 	res.status(status);
 	res.json(content);
-}
+};
+
+var theEarth = (function() {
+	var earthRadius = 6371; // km, miles is 3959
+
+	var getDistanceFromRads = function(rads) {
+		return parseFloat(rads * earthRadius);
+	};
+
+	var getRadsFromDistance = function(distance) { 
+		return parseFloat(distance/ earthRadius);
+	};
+
+	return {
+		getDistanceFromRads : getDistanceFromRads,
+		getRadsFromDistance : getRadsFromDistance
+	};
+})();
 
 module.exports.locationsListByDistance = function (req, res) { 
-	sendJsonResponse(res, 200, {"status" : "success"})
+	var lng = parseFloat(req.query.lng);
+	var lat = parseFloat(req.query.lat);
+	var maxDistance = parseFloat(req.query.maxDistance);
+	var point = {
+		type: "Point",
+		coordinates: [lng, lat]
+	};
+	var geoOptions = {
+		spherical: true,
+		maxDistance: theEarth.getRadsFromDistance(maxDistance),
+		num: 10
+	};
+	if (!lng || !lat || !maxDistance) {
+		sendJsonResponse(res, 404, {
+			"message": "lng, lat, and maxDistance query paremeters are all required"
+		});
+		return;
+	}
+	Loc.geoNear(point, geoOptions, function (err, results, stats) {
+		var locations = [];
+		if (err) {
+			sendJsonResponse(res, 404, err);
+		} else { 
+			results.forEach(function(doc) {
+			locations.push({
+				distance: theEarth.getDistanceFromRads(doc.dis),
+				name: doc.obj.name,
+				address: doc.obj.address,
+				rating: doc.obj.rating,
+				facilities: doc.obj.factilities,
+				_id: doc.obj._id
+				});
+			});
+			sendJsonResponse(res, 200, locations);
+		}
+	});
 };
 
 module.exports.locationsCreate = function (req, res) { 
@@ -23,8 +75,9 @@ module.exports.locationsReadOne = function (req, res) {
 					sendJsonResponse(res, 404, {
 						"message": "locationid not found"
 					});	
+					return;
 				} else if (err) {
-					sendJsonResponse(res, 404, err);
+					sendJsonResponse(res, 400, err);
 					return;
 				}
 				sendJsonResponse(res, 200, location);
